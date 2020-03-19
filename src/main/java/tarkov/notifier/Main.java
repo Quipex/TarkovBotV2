3088c8cad9e2cd157ae0ec47fa23d82d4c1bf45f
Quipex <rudas72@gmail.com>
2020-03-19 16:38:51 +0200

    added sleep log

--- src/main/java/tarkov/notifier/Main.java
+++ src/main/java/tarkov/notifier/Main.java
@@ -1,13 +1,18 @@
 package tarkov.notifier;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
     public static final int MINUTES_BETWEEN_CYCLES = Integer.parseInt(Configuration.getOSVariable("MINUTES_BETWEEN_CYCLES"));
 
     public static void main(String[] args) throws InterruptedException {
         Worker worker = new Worker();
+        //noinspection InfiniteLoopStatement
         while (true) {
             worker.work();
+            log.info("Sleeping for " + MINUTES_BETWEEN_CYCLES + " minutes.");
             Thread.sleep(MINUTES_BETWEEN_CYCLES * 60 * 1000);
         }
     }
