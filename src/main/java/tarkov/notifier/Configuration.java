433b6afa4da36db6ffa5561c42af358ac6c1592e
Quipex <rudas72@gmail.com>
2020-03-19 16:00:57 +0200

    fix for sysenv

--- src/main/java/tarkov/notifier/Configuration.java
+++ src/main/java/tarkov/notifier/Configuration.java
@@ -2,6 +2,6 @@ package tarkov.notifier;
 
 public interface Configuration {
     static String getOSVariable(String key) {
-        return System.getProperty(key);
+        return System.getenv(key);
     }
 }
