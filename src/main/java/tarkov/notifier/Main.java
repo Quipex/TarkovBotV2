9adb7b43ea727bc0dcc7aa523d7222fa8a5a5d32
Quipex <rudas72@gmail.com>
2020-03-23 00:12:37 +0200

    disabled unnecessary logging and added fallback send error

--- src/main/java/tarkov/notifier/Main.java
+++ src/main/java/tarkov/notifier/Main.java
@@ -1,6 +1,13 @@
 package tarkov.notifier;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Slf4j
 public class Main {
@@ -8,12 +15,27 @@ public class Main {
     public static final int MINUTES_BETWEEN_CYCLES = Integer.parseInt(Configuration.getOSVariable("MINUTES_BETWEEN_CYCLES"));
 
     public static void main(String[] args) throws InterruptedException {
+        disableLoggers();
         Worker worker = new Worker();
         //noinspection InfiniteLoopStatement
         while (true) {
-            worker.work();
+            try {
+                worker.work();
+            } catch (Exception e) {
+                Telegram.sendError(e);
+            }
             log.info("Sleeping for " + MINUTES_BETWEEN_CYCLES + " minutes.");
             Thread.sleep(MINUTES_BETWEEN_CYCLES * 60 * 1000);
+        }
+    }
+
+    private static void disableLoggers() {
+        Set<String> loggers = new HashSet<>(Arrays.asList("org.apache.http", "groovyx.net.http"));
+
+        for (String log : loggers) {
+            var logger = (Logger) LoggerFactory.getLogger(log);
+            logger.setLevel(Level.INFO);
+            logger.setAdditive(false);
         }
     }
 }
